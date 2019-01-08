121070822f0bda76f4364d79da9e35d626e3131e
Conor Patrick <conorpp94@gmail.com>
2019-01-07 21:20:07 -0500

    Update main.c

--- fido2/main.c
+++ fido2/main.c
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2018 SoloKeys, Inc. <https://solokeys.com/>
- * 
+ *
  * This file is part of Solo.
- * 
+ *
  * Solo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Solo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Solo.  If not, see <https://www.gnu.org/licenses/>
- * 
+ *
  * This code is available under licenses for commercial use.
  * Please contact SoloKeys for more information.
  */
@@ -46,6 +46,7 @@ int main(int argc, char * argv[])
             // TAG_GA |
             // TAG_WALLET |
             TAG_STOR |
+            TAG_NFC |
             // TAG_CP |
             // TAG_CTAP|
            // TAG_HID|
@@ -95,6 +96,7 @@ int main(int argc, char * argv[])
         {
         }
         ctaphid_check_timeouts();
+
     }
 
     // Should never get here
