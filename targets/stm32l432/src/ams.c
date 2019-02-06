4611f05051dfe6a17fb85f801c7fca65635ed041
merlokk <807634+merlokk@users.noreply.github.com>
2019-02-06 15:17:52 +0200

    small fix in AMS3956 debug texts

--- targets/stm32l432/src/ams.c
+++ targets/stm32l432/src/ams.c
@@ -277,10 +277,10 @@ bool ams_init()
 	uint8_t productType = ams_read_reg(AMS_REG_PRODUCT_TYPE);
 	if (productType != 0x14)
 	{
-		printf1(TAG_NFC,"Have no product type [0x%02x]. Connection error.\n", productType);
+		printf1(TAG_NFC, "Have no product type [0x%02x]. AMS3956 connection error.\n", productType);
 		return false;
 	}
-	printf1(TAG_NFC,"Product type 0x%02x.\n", productType);
+	printf1(TAG_NFC,"AMS3956 product type 0x%02x.\n", productType);
 	
     // enable tunneling mode and RF configuration
     ams_write_reg(AMS_REG_IC_CONF2, AMS_RFCFG_EN | AMS_TUN_MOD);
