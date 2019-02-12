ed676151f13b2b9ae8176b3318c9f49c4897d783
Conor Patrick <conorpp94@gmail.com>
2019-02-12 17:18:17 -0500

    update license to apache2 + mit

--- tools/ctap_test.py
+++ tools/ctap_test.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2018 SoloKeys, Inc. <https://solokeys.com/>
+# Copyright 2019 SoloKeys Developers
 #
-# This file is part of Solo.
-#
-# Solo is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Solo is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Solo.  If not, see <https://www.gnu.org/licenses/>
-#
-# This code is available under licenses for commercial use.
-# Please contact SoloKeys for more information.
+# Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+# http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+# http://opensource.org/licenses/MIT>, at your option. This file may not be
+# copied, modified, or distributed except according to those terms.
 #
 
 # Script for testing correctness of CTAP2/CTAP1 security token
