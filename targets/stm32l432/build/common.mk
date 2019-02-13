650e0b26cc836b7c5e13fed3bf958710f1dda741
Conor Patrick <conorpp94@gmail.com>
2019-02-12 20:29:33 -0500

    change assignment, else this evaluates for every file

--- targets/stm32l432/build/common.mk
+++ targets/stm32l432/build/common.mk
@@ -3,11 +3,11 @@ CP=$(PREFIX)arm-none-eabi-objcopy
 SZ=$(PREFIX)arm-none-eabi-size
 AR=$(PREFIX)arm-none-eabi-ar
 
-VERSION=$(shell git describe --abbrev=0 )
-VERSION_FULL=$(shell git describe)
-VERSION_MAJ=$(shell python -c 'print("$(VERSION)".split(".")[0])')
-VERSION_MIN=$(shell python -c 'print("$(VERSION)".split(".")[1])')
-VERSION_PAT=$(shell python -c 'print("$(VERSION)".split(".")[2])')
+VERSION:=$(shell git describe --abbrev=0 )
+VERSION_FULL:=$(shell git describe)
+VERSION_MAJ:=$(shell python -c 'print("$(VERSION)".split(".")[0])')
+VERSION_MIN:=$(shell python -c 'print("$(VERSION)".split(".")[1])')
+VERSION_PAT:=$(shell python -c 'print("$(VERSION)".split(".")[2])')
 
 VERSION_FLAGS= -DSOLO_VERSION_MAJ=$(VERSION_MAJ) -DSOLO_VERSION_MIN=$(VERSION_MIN) \
 	-DSOLO_VERSION_PATCH=$(VERSION_PAT) -DVERSION=\"$(VERSION_FULL)\"
