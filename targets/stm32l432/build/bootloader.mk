33749acafb6ab36ee6beca3b1f7a085447903b11
Conor Patrick <conorpp94@gmail.com>
2019-05-09 02:44:17 -0400

    add to bootloader

--- targets/stm32l432/build/bootloader.mk
+++ targets/stm32l432/build/bootloader.mk
@@ -3,7 +3,7 @@ include build/common.mk
 # ST related
 SRC = bootloader/main.c bootloader/bootloader.c
 SRC += src/init.c src/redirect.c src/flash.c src/rng.c src/led.c src/device.c
-SRC += src/fifo.c src/crypto.c src/attestation.c
+SRC += src/fifo.c src/crypto.c src/attestation.c src/sense.c
 SRC += src/startup_stm32l432xx.s src/system_stm32l4xx.c
 SRC += $(DRIVER_LIBS) $(USB_LIB)
 
