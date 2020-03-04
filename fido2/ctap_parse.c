5e6486ef152b479998776e2b5e333efe4730da3b
Radoslav Gerganov <rgerganov@vmware.com>
2020-03-04 14:57:33 +0200

    Check return values when parsing CTAP commands

--- fido2/ctap_parse.c
+++ fido2/ctap_parse.c
@@ -871,7 +871,7 @@ uint8_t ctap_parse_make_credential(CTAP_makeCredential * MC, CborEncoder * encod
         {
             return ret;
         }
-        cbor_value_advance(&map);
+        ret = cbor_value_advance(&map);
         check_ret(ret);
     }
 
@@ -1132,7 +1132,7 @@ uint8_t ctap_parse_get_assertion(CTAP_getAssertion * GA, uint8_t * request, int 
             return ret;
         }
 
-        cbor_value_advance(&map);
+        ret = cbor_value_advance(&map);
         check_ret(ret);
     }
 
